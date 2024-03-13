056d452d2719965eaa98ba74847458c884f4b86c
Yuri Serafim <yuridefranca@gmail.com>
2024-03-13 11:13:14 +0000

    test: :white_check_mark: fix addition feature test

--- math.spec.mjs
+++ math.spec.mjs
@@ -2,9 +2,9 @@ import { describe, it } from 'node:test';
 import assert from 'node:assert';
 import * as math from './math.mjs';
 
-describe('sum', () => {
+describe('addition', () => {
 	it('adds 1 + 2 to equal 3', () => {
-		const result = math.sum(1, 2);
+		const result = math.addition(1, 2);
 		const expected = 3;
 
 		assert.equal(result, expected);
